962bfafac947b331f886214fec99bff3b3f42713
Samuelwanyinge05 <88209457+Samuelwanyinge05@users.noreply.github.com>
2023-10-16 12:03:06 +0300

    Add files via upload

---
+++ gradding system 2/gradding system 2/Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        ListBox1 = New ListBox()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 27)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(103, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Answer Key"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(244, 27)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(293, 342)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(101, 31)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(525, 323)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(111, 50)
+        Button1.TabIndex = 4
+        Button1.Text = "Exit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(51, 323)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(126, 50)
+        Button2.TabIndex = 5
+        Button2.Text = "Grade"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 25
+        ListBox1.Location = New Point(25, 87)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(417, 179)
+        ListBox1.TabIndex = 6
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(301, 302)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 25)
+        Label2.TabIndex = 7
+        Label2.Text = "Average"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(ListBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "gradding program"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ gradding system 2/gradding system 2/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports grading_system_2.Form1
+Imports System.IO
+
+Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+
+
+
+        Dim answerKey As String = File.ReadAllText("C:\Users\Administrator\source\repos\gradding system 2\Answer_key.txt")
+        TextBox1.Text = answerKey
+
+
+
+        Dim studentRecords As New List(Of StudentRecord)()
+        For Each line As String In File.ReadLines("C:\Users\Administrator\source\repos\gradding system 2\student.txt")
+            Dim parts() As String = line.Split(","c)
+            If parts.Length = 2 Then
+                Dim student As New StudentRecord With {.Name = parts(0), .Answers = parts(1)}
+                studentRecords.Add(student)
+            End If
+        Next
+
+        Dim totalStudents As Integer = 0
+        Dim totalCorrectAnswers As Integer = 0
+
+        For Each student As StudentRecord In studentRecords
+            Dim correctAnswers As Integer = CompareAnswers(student.Answers, answerKey)
+            Dim letterGrade As Char = CalculateLetterGrade(correctAnswers)
+
+            ListBox1.Items.Add($"{student.Name},    {correctAnswers},   {letterGrade}")
+
+            totalStudents += 1
+            totalCorrectAnswers += correctAnswers
+        Next
+
+        ListBox1.Items.Add($"Total Students: {totalStudents}, Total Correct Answers: {totalCorrectAnswers}")
+
+        Dim averageCorrectAnswer As Double = If(totalStudents > 0, totalCorrectAnswers / totalStudents, 0)
+        TextBox2.Text = $" {averageCorrectAnswer:F2}"
+
+
+
+
+
+    End Sub
+
+    Private Function CompareAnswers(studentAnswers As String, answerKey As String) As Integer
+        If studentAnswers.Length <> answerKey.Length Then
+            Return 0 ' Return 0 for invalid answers
+        End If
+
+        Dim correctAnswers As Integer = 0
+
+        For i As Integer = 0 To studentAnswers.Length - 1
+            If studentAnswers(i) = answerKey(i) Then
+                correctAnswers += 1
+            End If
+        Next
+
+        Return correctAnswers
+    End Function
+
+    Private Function CalculateLetterGrade(correctAnswers As Integer) As Char
+        If correctAnswers >= 9 Then
+            Return "A"c
+        ElseIf correctAnswers >= 8 Then
+            Return "B"c
+        ElseIf correctAnswers >= 7 Then
+            Return "C"c
+        ElseIf correctAnswers >= 6 Then
+            Return "D"c
+        Else
+            Return "F"c
+        End If
+    End Function
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Exit()
+    End Sub
+End Class
+
+Public Class StudentRecord
+    Public Property Name As String
+    Public Property Answers As String
+End Class
+
+
+
+
+
+Public Class Student
+    Public Property Name As String
+    Public Property CorrectCount As Integer
+    Public Property LetterGrade As Char
+
+    Public Sub New(name As String, correctCount As Integer, letterGrade As Char)
+        Me.Name = name
+        Me.CorrectCount = correctCount
+        Me.LetterGrade = letterGrade
+    End Sub
+End Class
